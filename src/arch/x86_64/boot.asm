d5a2b1196d6db6e38308b0057e3a4e9a7e75d387
Yifan Qiao <326698794@qq.com>
2018-04-06 21:52:40 +0800

    add page table support (recursive P4 and normal P3-1)

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -23,6 +23,11 @@ start:
     hlt
 
 set_up_page_tables:
+    ; recursive map the page table
+    mov eax, p4_table
+    or eax, 0b11 ; present + writable
+    mov [p4_table + 511 * 8], eax
+    
     ; map first P4 entry to P3 table
     mov eax, p3_table
     or eax, 0b11 ; present + writable
--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,14 @@
 mod vga_buffer;
 mod memory;
 
+#[macro_use]
+extern crate bitflags;
+
 extern crate rlibc;
 extern crate volatile;
 extern crate spin;
 extern crate multiboot2;
+extern crate x86_64;
 
 #[no_mangle]
 pub extern fn rust_main(multiboot_information_address: usize) {
@@ -45,6 +49,22 @@ pub extern fn rust_main(multiboot_information_address: usize) {
     let multiboot_end = multiboot_start + (boot_info.total_size as usize);
     println!("kernel start: 0x{:x}, kernel end: 0x{:x}", {kernel_start}, {kernel_end});
     println!("multiboot start: 0x{:x}, multiboot end: 0x{:x}", {multiboot_start}, {multiboot_end});
+    
+    let mut frame_allocator = memory::AreaFrameAllocator::new(
+    kernel_start as usize, kernel_end as usize, multiboot_start,
+    multiboot_end, memory_map_tag.memory_areas());
+    // use memory::FrameAllocator;
+    // for i in 0.. {
+    //     use memory::FrameAllocator;
+    //     // println!("{:?}", frame_allocator.allocate_frame());
+    //     if let None = frame_allocator.allocate_frame() {
+    //         println!("allocated {} frames", i);
+    //         break;
+    //     }
+    // }
+
+    memory::test_paging(&mut frame_allocator);
+
     loop{}
 }
 
