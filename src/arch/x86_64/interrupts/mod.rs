799db9c7fa578976b7004f60a0ab5610e8849c01
Yifan Qiao <326698794@qq.com>
2018-04-26 16:26:10 +0800

    add some instructions for interrupts

--- src/arch/x86_64/interrupts/mod.rs
+++ src/arch/x86_64/interrupts/mod.rs
@@ -21,3 +21,36 @@ pub fn enable_irq(irq: u8) {
         pic::enable_irq(irq);
     }
 }
+
+/// Set interrupts and halt
+/// This will atomically wait for the next interrupt
+/// Performing enable followed by halt is not guaranteed to be atomic, use this instead!
+#[inline(always)]
+pub unsafe fn enable_and_halt() {
+    asm!("sti
+        hlt"
+        : : : : "intel", "volatile");
+}
+
+/// Set interrupts and nop
+/// This will enable interrupts and allow the IF flag to be processed
+/// Simply enabling interrupts does not gurantee that they will trigger, use this instead!
+#[inline(always)]
+pub unsafe fn enable_and_nop() {
+    asm!("sti
+        nop"
+        : : : : "intel", "volatile");
+}
+
+/// Halt instruction
+#[inline(always)]
+pub unsafe fn halt() {
+    asm!("hlt" : : : : "intel", "volatile");
+}
+
+/// Pause instruction
+/// Safe because it is similar to a NOP, and has no memory effects
+#[inline(always)]
+pub fn pause() {
+    unsafe { asm!("pause" : : : : "intel", "volatile"); }
+}
